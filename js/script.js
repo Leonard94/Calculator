e64d909215bc0c410f6ad16f58dbccab4bde3156
Leonard94 <vladisla.koleda@yandex.ru>
2021-11-26 17:03:34 +0300

    Теперь калькулятор выполняет основные арифметические операции. работает только в console.log

--- js/script.js
+++ js/script.js
@@ -9,18 +9,14 @@ body.addEventListener('click', (e) => {
 })
 
 
-
 /*
 Алгоритм:
-1. Значение булевой переменной isWaitingNextValue устанавливаем false, что означает - еще не вводили цифры
-2. Вводим первый операнд - значение попадает в currentNumber
-3. При нажатии на любой оператор (+, -, ×, ÷, =), при isWaitingNextValue = false - заносим его в переменную operator
-4. А вводимый ранее операнд (первый) попадает в переменную firstNumber
-6. Вводим второй операнд, вводимое значение заносится в currentNumber
-7. При нажатии на любой оператор (+, -, ×, ÷, =), получаем результат и заносим его в currentNumber
+1. Вводим число, каждая вводимая цифра попадает в currentOperand
+2. При нажатии на любой operator, что означает - ввод операнда окончен, в переменную operator заносится нажатый оператор. Раннее вводимое число заносится в firstOperand, а currentOperand очищается. 
+3. Вводим второй операнд, каждая вводимая цифра попадает в currentOperand
+4. При следующем нажатии на любой оператор (или равно), считается результат и заносится в firstOperand, а currentOperand очищается
+5. И так по кругу
 */
-
-
 
 
 const result = document.querySelector('.result')
@@ -28,26 +24,28 @@ const btns = document.querySelector('.btns')
 const operand = document.querySelector('.operand')
 
 
-let currentNumber = ''
-let firstNumber = 0
+let currentOperand = ''
+let firstOperand = 0
 let currentOperator = ''
 let isWaitingNextValue = false
 
 btns.addEventListener('click', (e) => {
-    if (e.target.getAttribute('value') !== null) setCurrentNumber(e.target.getAttribute('value'))
+    if (e.target.getAttribute('value') !== null) setCurrentOperand(e.target.getAttribute('value'))
     if (e.target.getAttribute('operator') !== null) setOperator(e.target.getAttribute('operator'))
     if (e.target.getAttribute('operation') === 'reset') resetBtn()
+    if (e.target.getAttribute('operation') === 'calc') calcResult()
     if (e.target.getAttribute('operation') === 'delete') deleteBtn()
 
-    console.log(`currentNumber: ${currentNumber}`)
-    console.log(`firstNumber: ${firstNumber}`)
+    // Для тестирования
+    console.log(`currentOperand: ${currentOperand}`)
+    console.log(`firstOperand: ${firstOperand}`)
     console.log(`currentOperator: ${currentOperator}`)
     console.log('    ')
 })
 
-// Вводим число
-const setCurrentNumber = (number) => {
-    currentNumber += number
+// При вводе числа
+const setCurrentOperand = (number) => {
+    currentOperand += number
     isWaitingNextValue = false
 }
 
@@ -58,19 +56,48 @@ const setOperator = (operator) => {
         currentOperator = operator
         return
     }
-    if (firstNumber === 0) {
-        console.log('Запомнили первое число')
-        firstNumber = currentNumber
-        currentNumber = ''
-        // currentOperator = operator
+    if (firstOperand === 0) {
+        // Если в firstOperand пусто, то заносим туда данные из currentOperand
+        firstOperand = currentOperand
+        currentOperand = ''
+        currentOperator = operator
     } else {
-        // Посчитать результаты
-        console.log('Считаем результаы')
         isWaitingSecondNumber = true
-        operators[operator](firstNumber, currentNumber)
+        // Считаем и заносим результат в переменную currentOperand на случай, если дальше продолжим считать
+        // Результат получаем в number, переводим в строку.
+        firstOperand = String(calculateResult[operator](firstOperand, currentOperand))
+        currentOperand = ''
     }
     isWaitingNextValue = true
     currentOperator = operator
+}
+
+
+const calculateResult = {
+    '+': () => {
+        console.log('Выполняем сложение')
+        return Number(firstOperand) + Number(currentOperand)
+    },
+    '-': () => {
+        console.log('Выполняем вычитание')
+        return Number(firstOperand) - Number(currentOperand)
+    },
+    '*': () => {
+        console.log('Выполняем умножение')
+        return Number(firstOperand) * Number(currentOperand)
+    },
+    '/': () => {
+        console.log('Выполняем деление')
+        return Number(firstOperand) / Number(currentOperand)
+    }
+}
+
+const calcResult = () => {
+    console.log('Равно')
+    
+    if(firstOperand === 0) return
+    firstOperand = String(calculateResult[currentOperator](firstOperand, currentOperand))
+    currentOperand = ''
 }
 
 const deleteBtn = () => {
@@ -81,16 +108,6 @@ const resetBtn = () => {
     console.log('reset')
 }
 
-const operators = {
-    '+': (firstNumber, currentNumber) => {
-        console.log('Выполнили сложение')
-        return Number(firstNumber) + Number(currentNumber)
-    },
-    '-': () => console.log('Выполнили вычитание'),
-    '*': () => console.log('Выполнили умножение'),
-    '/': () => console.log('Выполнили деление'),
-    '=': () => console.log('Нажали равно'),
-}
 
 
 
